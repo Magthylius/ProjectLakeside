9086b832e5a65b6dafd22e8667fc690974ec72e7
Magthylius <56510076+Magthylius@users.noreply.github.com>
2023-08-31 15:58:31 +0800

    chore: project init

---
+++ ProjectLakeside/Source/ProjectLakeside/ProjectLakesideGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectLakesideGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectLakesideGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectLakesideGameMode();
+};
+
+
+
